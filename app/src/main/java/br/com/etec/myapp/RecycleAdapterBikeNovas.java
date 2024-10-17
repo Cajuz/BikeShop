18c50e1b7faf5e46fdd122bb7e73d0b99a3efaf9
Cajuz <alvaroalvi57@gmail.com>
2024-10-17 20:18:33 -0300

    Montados adptaer que ira chamar a lista e os metodos

--- app/src/main/java/br/com/etec/myapp/RecycleAdapterBikeNovas.java
+++ app/src/main/java/br/com/etec/myapp/RecycleAdapterBikeNovas.java
@@ -1,31 +1,53 @@
 package br.com.etec.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecycleAdapterBikeNovas extends RecyclerView.Adapter<RecycleAdapterBikeNovas.ViewHolder> {
+import java.util.List;
 
+public class RecycleAdapterBikeNovas extends RecyclerView.Adapter<RecycleAdapterBikeNovas.ViewHolder> {
+    //variaveis que representam a lista e o contexto
+    private Context context;
+    private List<BikeNova> lstBikeNova;
+
+    //crianco o construtor
+
+
+    public RecycleAdapterBikeNovas(Context context, List<BikeNova> lstBikeNova) {
+        this.context = context;
+        this.lstBikeNova = lstBikeNova;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_bikes_novas,parent,false);
+
+        return new ViewHolder(view);
     }
 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.idModeloTituloBikeNova.setText(lstBikeNova.get(position.getTitulo()));
+        holder.idModeloImagemBikeNova.setImageResource(lstBikeNova.get(position.getImagem()));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstBikeNova.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
